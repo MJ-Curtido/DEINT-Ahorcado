be6c35b468cfae3a1710fa9a0a379b6bb5e0cb5e
MJ-Curtido <manu17pc@gmail.com>
2023-01-11 09:17:43 +0100

    comprobando letras

--- DEINT-Ahorcado/MainPage.xaml.cs
+++ DEINT-Ahorcado/MainPage.xaml.cs
@@ -4,20 +4,56 @@ namespace DEINT_Ahorcado;
 
 public partial class MainPage : ContentPage
 {
-	Ahorcado ahorcado;
-	int count = 0;
+	private Ahorcado ahorcado;
+    private int count = 0;
+	private String palabraSecreta;
+	private String palabraGuion;
+	private int contFallos;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
 		ahorcado = new Ahorcado();
-
+		
 		BindingContext = ahorcado;
+
+        palabraSecreta = ahorcado.getPalabraSecreta();
+		palabraGuion = "";
+		for (int i = 0; i < palabraSecreta.Length; i++)
+		{
+			palabraGuion += "_";
+		}
+
+		contFallos = 0;
     }
 
-	private void Button_Clicked(object sender, EventArgs e)
+    private void Button_Clicked(object sender, EventArgs e)
 	{
+		Button btn = (Button)sender;
 
+		if (!comprobarLetra(btn.Text[0]))
+		{
+			contFallos++;
+			imgAhorcado.Source = "img" + contFallos + ".jpg";
+		}
+
+        btn.IsEnabled = false;
+    }
+
+	private Boolean comprobarLetra(Char letra)
+	{
+		Boolean esta = false;
+
+		for (int i = 0; i < palabraSecreta.Length; i++)
+		{
+			if (palabraSecreta[i] == letra)
+			{
+				palabraGuion.Insert(i, letra + "").Remove(i + 1, 1);
+				esta = true;
+			}
+		}
+
+		return esta;
 	}
 }
 
--- DEINT-Ahorcado/Model/Ahorcado.cs
+++ DEINT-Ahorcado/Model/Ahorcado.cs
@@ -8,13 +8,19 @@ namespace DEINT_Ahorcado.Model
 {
     public class Ahorcado
     {
-        public String palabraSecreta { get; set; }
+        Random rnd = new Random();
+        public List<String> palabras { get; set; } = new List<String>() { "dam", "estudiar", "programar", "ordenador", "teclado", "raton", "alfombrilla", "monitor" };
         public int fase { get; set; }
         public IEnumerable<Char> letras { get; set; } = new List<Char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
         public Ahorcado()
         {
 
+        }
+
+        public String getPalabraSecreta()
+        {
+            return palabras.OrderBy(a => rnd.Next()).ToList().First();
         }
     }
 }
